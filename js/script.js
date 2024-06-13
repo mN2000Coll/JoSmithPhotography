17dad0174ebb0a2a6d74c31bd098f3aa21ada1fd
mN2000Coll <mateuszno2000@gmail.com>
2024-06-13 20:01:29 +0100

    Second commit

--- js/script.js
+++ js/script.js
@@ -14,3 +14,42 @@ function navShow() {
   }
   });
 
+  let API_KEY = "GJtgsPlioJ_miak48x4uvtYkVO-YdkCo03BDR4197pI";
+  let endPoint = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}`
+  
+  // Set reference variables to the necessary id'z
+  let unsplashIMG = document.querySelector('#unsplash_IMG');
+  let imageURL = document.querySelector('#img_URL');
+  let IMGcreator = document.querySelector('#IMG_creator');
+  
+  let btn = document.querySelector('.btn');
+  btn.addEventListener('click', function() {
+      clearTimeout(clearTimer);
+  })
+  let clearTimer;
+  
+  let fetchImage = () => {
+      clearTimer = setInterval(() => {
+          
+      
+      //fetch data from the server
+      fetch(endPoint)
+          .then((response) => {
+              return response.json();
+          })
+          .then((jsonData) => {
+              //Sets the image using the JSON data returned
+              unsplashIMG.src = jsonData.urls.regular;
+              // Sets the URL using the JSON data returned
+              imageURL.setAttribute('href', jsonData.links.html);
+              // Sets the creators name using the JSON data returned
+              IMGcreator.innerText = jsonData.user.name;
+              // Sets the creators URL using the JSON data returned
+              IMGcreator.setAttribute('href', jsonData.user.portfolio_url);
+          })
+          .catch((err) =>  {
+              console.log("Something Bad Happened " + err);
+          })
+      }, 5000)
+  }
+  fetchImage();
